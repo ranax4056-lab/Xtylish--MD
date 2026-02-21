944d967821a8eccd7365c231b397c067e741b780
ranax4056-lab <ranax4056@gmail.com>
2026-02-21 12:23:17 +0600

    Update main.js

--- main.js
+++ main.js
@@ -156,7 +156,7 @@ const channelInfo = {
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
             newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterName: 'SHAHIN RANA',
             serverMessageId: -1
         }
     }
